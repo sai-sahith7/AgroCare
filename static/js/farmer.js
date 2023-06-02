8680a6317593faa2f42c1ad2b26d63c979f3d171
Sai Sahith <saisahith.goud2021@vitstudent.ac.in>
2023-06-03 02:36:56 +0530

    initial commit

---
+++ static/js/farmer.js
@@ -0,0 +1,9 @@
+function onclick_test() {
+    document.getElementById("test-results").style.display = "block";
+    document.getElementById("book").style.display = "none";
+}
+
+function onclick_book() {
+    document.getElementById("test-results").style.display = "none";
+    document.getElementById("book").style.display = "block";
+}
---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+function onclick_signin() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("signin").style.display = "block";
+    document.getElementById("register").style.display = "none";
+}
+
+function onclick_home() {
+    document.getElementById("intro").style.display = "block";
+    document.getElementById("signin").style.display = "none";
+    document.getElementById("register").style.display = "none";
+}
+
+function onclick_register() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("signin").style.display = "none";
+    document.getElementById("register").style.display = "block";
+}
+
+
